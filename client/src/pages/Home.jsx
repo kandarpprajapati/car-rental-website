f59bef617a9fbcc5f49969d32e8138d59ce584a0
Kandarp <kandarpprajapati99@gmail.com>
2024-11-28 17:36:39 +0530

    hero section basic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import CarImage from "../../public/car-images/van.jpg";
 
 const Home = () => {
-  return <div>Hello</div>;
+  return (
+    <div className="relative bg-orange-500 rounded-lg shadow-md overflow-hidden max-w-7xl mx-auto mt-8">
+      {/* Image Section */}
+      <div className="relative">
+        <img
+          src={CarImage} // Replace with your car image
+          alt="Car"
+          className="w-full object-cover"
+        />
+      </div>
+
+      {/* Content Section */}
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-white text-center">
+        {/* <h1 className="text-3xl font-bold uppercase tracking-wider">
+          Helsinki
+        </h1>
+        <p className="text-lg mt-2">
+          Luotettavat palvelut <br /> Edulliseen hintaan.
+        </p> */}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
