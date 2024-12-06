252c9168cb0a9c9540dc7426beb17371dc35f043
Kandarp <kandarpprajapati99@gmail.com>
2024-12-06 14:58:04 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,21 +3,20 @@ import React, { useEffect } from "react";
 import CarImage from "../../public/car-images/van.png";
 import CarDetailsDialog from "./homepage/CarDetailsDialog.jsx";
 import { useGetProducts } from "../hooks/products/useGetProducts.js";
+import { useState } from "react";
 
 const Home = () => {
-  const cars = new Array(9).fill({
-    title: "Muutto ja Kuljetus",
-    price: "€39.99",
-    image: CarImage, // Replace with your actual image path
-  });
+  const [products, setProducts] = useState([]);
 
-  const {
-    data: { products, pagination },
-  } = useGetProducts();
+  const { data, isLoading, isFetching } = useGetProducts();
+
+  const loading = isLoading || isFetching;
 
   useEffect(() => {
-    console.log(products);
-  }, [products]);
+    if (data && data.products) {
+      setProducts(data.products);
+    }
+  }, [data]);
 
   return (
     <div className="max-w-screen-2xl mx-auto px-3 lg:px-2 xl:px-0">
@@ -43,37 +42,34 @@ const Home = () => {
         </div>
         <div className="mx-auto p-6 md:p-14 bg-primary-foreground rounded-2xl md:rounded-[50px]">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            {products.map((product, index) => (
-              <div
-                key={index}
-                className="bg-background rounded-lg overflow-hidden shadow-md hover:shadow-lg transform transition duration-300 ease-in-out p-4"
-              >
-                {/* Image Section */}
-                <div className="bg-gray flex justify-center rounded-lg">
-                  <img
-                    src={product.imageUrl}
-                    alt={product.name}
-                    className="rounded-lg"
-                  />
-                </div>
+            {!loading &&
+              products.length > 0 &&
+              products.map((product, index) => (
+                <div
+                  key={index}
+                  className="bg-background rounded-lg overflow-hidden shadow-md hover:shadow-lg transform transition duration-300 ease-in-out p-4"
+                >
+                  <div className="bg-gray flex justify-center rounded-lg">
+                    <img
+                      src={product.imageUrl}
+                      alt={product.name}
+                      className="rounded-lg"
+                    />
+                  </div>
 
-                {/* Content Section */}
-                <div className="p-4">
-                  <h3 className="text-primary-foreground text-lg font-semibold mb-20 uppercase">
-                    {product.name}
-                  </h3>
-                  <div className="flex justify-between items-center">
-                    <span className="text-secondary text-lg font-bold">
-                      {product.pricePerHour} {"/"} <sup>hr</sup>
-                    </span>
-                    {/* <button className="bg-secondary-foreground text-background text-sm py-1 px-4 rounded-full hover:bg-secondary">
-                      + Add
-                    </button> */}
-                    <CarDetailsDialog product={product} />
+                  <div className="p-4">
+                    <h3 className="text-primary-foreground text-lg font-semibold mb-20 uppercase">
+                      {product.name}
+                    </h3>
+                    <div className="flex justify-between items-center">
+                      <span className="text-secondary text-lg font-bold">
+                        {product.pricePerHour} {"/"} <sup>hr</sup>
+                      </span>
+                      <CarDetailsDialog product={product} />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </section>
